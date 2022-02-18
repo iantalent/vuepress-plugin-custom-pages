490e73cff1dbdc2027ada57e410ee68010b0ea83
iantalent <iantalent95@gmail.com>
2022-02-18 20:05:48 +0200

    few new tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,6 +1,7 @@
 import {suite, test} from '@testdeck/mocha';
 import * as chai from 'chai';
-import {isPage, SimplePage} from "../lib/page";
+import {isPage, SimplePage} from "../lib";
+import {Heading, Paragraph, UnorderedList} from "markdown-generator";
 
 const assert = chai.assert;
 
@@ -9,7 +10,37 @@ class CustomPageTests
 {
 	@test 'isPage'()
 	{
-		const page = new SimplePage('test', '/path');
+		const page = new SimplePage('a', '/a');
 		assert.isTrue(isPage(page));
+	}
+	
+	@test 'page parameters'()
+	{
+		const page = new SimplePage('name', '/path/', {
+			title: 'title',
+			metaTitle: 'metaTitle'
+		});
+		const frontMatter = page.frontmatter();
+		assert.equal(page.name(), 'name');
+		assert.equal(page.path(), '/path/');
+		assert.equal(frontMatter.title, 'title');
+		assert.equal(frontMatter.metaTitle, 'metaTitle');
+		assert.isUndefined(frontMatter.canonicalUrl);
+		assert.isUndefined(frontMatter.description);
+	}
+	
+	@test 'page tree'()
+	{
+		const page = new SimplePage('heading', '/path/');
+		page.add(new Paragraph('paragraph'))
+			.add((new UnorderedList()).add('first').add('second'));
+		const tree = page.tree();
+		
+		assert.isArray(tree);
+		assert.deepEqual(tree, [
+			new Heading('heading', 1),
+			new Paragraph('paragraph'),
+			(new UnorderedList()).add('first').add('second')
+		])
 	}
 }
